a3d37e1c613fb34a761655293dcc1fcf6511f0de
arthaud-proust <arthaudp33@gmail.com>
2023-11-15 00:50:11 +0100

    fix: socket connection

--- front/socket.ts
+++ front/socket.ts
@@ -5,4 +5,7 @@ import {
 import { Socket, io } from 'socket.io-client';
 
 type ClientSocket = Socket<IServerToClientEvents, IClientToServerEvents>;
-export const socket: ClientSocket = io('ws://localhost:3010');
+export const socket: ClientSocket = io('ws://localhost:3010', {
+  reconnection: true,
+  reconnectionDelay: 1000,
+});
