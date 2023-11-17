6df349bbcd13bdde065c568a1004c4346e516571
Hashjolly <jolly.hugo@hotmail.com>
2023-11-17 16:04:22 +0100

    Demons are visible onMouseEnter but every players can see every demons

--- front/components/card/CoveredDemonCard.tsx
+++ front/components/card/CoveredDemonCard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { socket } from '@/socket';
 import { CardData } from '@lsd/back/contracts/card';
 import { Card } from './Card';
@@ -10,6 +11,8 @@ export const CoveredDemonCard = ({
   cardData,
   isSummonable,
 }: CoveredDemonCard) => {
+  const [isCovered, setIsCovered] = useState(false);
+
   const summonDemon = () => {
     if (!isSummonable) {
       return;
@@ -18,9 +21,12 @@ export const CoveredDemonCard = ({
   };
 
   return (
-    <article>
+    <article
+      onMouseEnter={() => setIsCovered(true)}
+      onMouseLeave={() => setIsCovered(false)}
+    >
       {isSummonable && <button onClick={summonDemon}>Summon</button>}
-      <Card covered={true} cardData={cardData} />
+      <Card covered={!isCovered} cardData={cardData} />
     </article>
   );
 };
