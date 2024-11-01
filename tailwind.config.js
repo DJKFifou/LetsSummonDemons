9b19e26cefb3bd15115d5f8b76dca840c62903b4
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2024-11-01 16:44:48 +0100

    add tailwind with preconfig

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./front/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#0F0E09',
+        red: '#D83928',
+        yellow: '#FFC800',
+      },
+      // fontFamily: {
+      //   title: ['BaiJamjuree', 'sans-serif'],
+      //   body: ['Noka', 'sans-serif'],
+      // },
+      screens: {
+        '4xs': '320px',
+        '3xs': '380px',
+        '2xs': '430px',
+        xs: '530px',
+      },
+      fontSize: {
+        '2xs': ['0.625rem', '0.875rem'],
+        '3.5xl': ['2rem', '2.5rem'],
+        '4.5xl': ['2.5rem', '2.813rem'],
+        '5.5xl': ['3.375rem', '3.375rem'],
+        '6.5xl': ['4rem', '3.4rem'],
+        '8.5xl': ['7rem', '7.313rem'],
+      },
+      maxWidth: {
+        screen: '100vw',
+      },
+    },
+  },
+  plugins: [],
+};
