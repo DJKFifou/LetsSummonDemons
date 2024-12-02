543d40539fa15672045fa1c780d8686f294eb743
Maxime <lust.maxime@outlook.fr>
2024-12-02 12:04:33 +0100

    update z-index translationButtons

--- front/components/layout/TranslationButtons.tsx
+++ front/components/layout/TranslationButtons.tsx
@@ -16,7 +16,7 @@ export const TranslationButtons = () => {
 
   return (
     <article>
-      <div className="absolute top-0 right-0 flex justify-end space-x-4">
+      <div className="absolute top-0 right-0 flex justify-end gap-4 z-10">
         <button
           onClick={() => changeLanguage('fr')}
           className="px-4 py-2 bg-blue-500 text-white rounded"
