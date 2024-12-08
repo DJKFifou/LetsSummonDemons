a1b04c9af388a7ed87bf27cbb446fed042f9864c
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2024-12-08 03:21:06 +0100

    update players.map for personal position in game

--- front/components/game/Game.tsx
+++ front/components/game/Game.tsx
@@ -15,9 +15,18 @@ export const GameDataDisplay = ({
   const { t } = useTranslation();
   const itsYourTurn = gameData.turn?.current.player.id === playerId;
   const isMarketOpen = itsYourTurn && !!gameData.turn?.current.canBuyNeighbor;
+
+  const positions = [
+    'left-0 bottom-0',
+    'left-1/4 top-0',
+    'right-1/4 top-0',
+    'left-0 top-1/4',
+    'right-0 top-1/4',
+  ];
+
   return (
-    <article className="flex flex-col gap-2 p-2 border">
-      <p>
+    <article className="flex flex-col gap-2 p-2">
+      {/* <p>
         <b>{t('game.game.game')}</b>
       </p>
       <p>
@@ -25,8 +34,8 @@ export const GameDataDisplay = ({
       </p>
       <p>
         {t('game.game.state')}: {gameData.state}
-      </p>
-      <div className="flex flex-col gap-2 p-2 border">
+      </p> */}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2 p-2">
         {gameData.neighborsDeck && (
           <NeighborsDeck
             gameData={gameData}
@@ -36,20 +45,27 @@ export const GameDataDisplay = ({
           />
         )}
       </div>
-      <p>{t('game.game.players')}:</p>
+      {/* <p>{t('game.game.players')}:</p> */}
       <div className="flex gap-2">
         {gameData.players
           .slice()
           .sort((a, b) => (a.id === playerId ? -1 : b.id === playerId ? 1 : 0))
-          .map((player) => (
-            <PlayerDataDisplay
-              gameData={gameData}
-              playerData={player}
-              itsTurn={gameData.turn?.current.player.id === player.id}
-              itsYou={player.id === playerId}
-              key={player.id}
-            />
-          ))}
+          .map((player, index) => {
+            const positionClass =
+              player.id === playerId ? positions[0] : positions[index];
+
+            return (
+              <div className={`absolute ${positionClass}`} key={player.id}>
+                <PlayerDataDisplay
+                  gameData={gameData}
+                  playerData={player}
+                  itsTurn={gameData.turn?.current.player.id === player.id}
+                  itsYou={player.id === playerId}
+                  key={player.id}
+                />
+              </div>
+            );
+          })}
       </div>
     </article>
   );
